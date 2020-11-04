6472e2925fd077730d4b0b09b46f141b5a612c6a
Myles Burt <myles_burt@rocketmail.com>
2020-11-04 21:06:36 +1030

    JS file for holding code for server created.

---
+++ server.js
@@ -0,0 +1,24 @@
+let express = require("express");
+
+let app = express();
+
+let PORT = process.env.PORT || 4040
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTES....
+
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+// app.get("/notes", function (req, res) {
+//     res.sendFile(path.join(__dirname, "notes.html")
+// })
+
+require("./Develop/routes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
