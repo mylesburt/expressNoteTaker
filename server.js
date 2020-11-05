701456fb0cce7d5cb4d5a7c1b79c8ed7e1186c07
Myles Burt <myles_burt@rocketmail.com>
2020-11-05 17:38:37 +1030

    require set up for html routes file.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-let express = require("express");
+const express = require("express");
 
-let app = express();
+const app = express();
 
-let PORT = process.env.PORT || 4040
+const PORT = process.env.PORT || 4040
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
