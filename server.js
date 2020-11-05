fc6dcb2e7fc6142d9e3022fc7170f2ef06f5b31c
Myles Burt <myles_burt@rocketmail.com>
2020-11-05 21:06:02 +1030

    Getting help.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 4040
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
 //ROUTES....
 
 // app.get("/", function (req, res) {
